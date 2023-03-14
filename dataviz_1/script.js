220da625362cc60001f67e099d05f0dfe611620b
Nanu Kissner <nadiakissner17@gmail.com>
2023-03-14 18:38:52 -0300

    commit ine linda

---
+++ dataviz_1/script.js
@@ -0,0 +1,20 @@
+
+d3.csv("astronautas.csv", d3.autoType).then((data) => {
+  console.log(data)
+  let chart = Plot.plot({
+    marks: [Plot.dot(data, { x: "edad", y: "mision_hs" })],
+    x: {
+      grid: true,
+      line: true,
+      zero: true,
+      nice: true,
+    },
+    y: {
+      zero: true,
+      nice: true,
+      line: true,
+      grid: true,
+    },
+  });
+  d3.select("#chart").append(() => chart);
+});
