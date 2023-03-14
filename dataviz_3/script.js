1b0bfe7136dec3b152a6a4e7b058035c9d2444fb
ines galarza <inegalarza@gmail.com>
2023-03-14 18:45:24 -0300

    commit 7

--- dataviz_3/script.js
+++ dataviz_3/script.js
@@ -2,13 +2,14 @@
 
 d3.csv('astronautas.csv', d3.autoType).then(data => {
     console.log(data)
+    
     let chart = Plot.plot({
       marks: [
         Plot.barY(data, {
           x: 'Genero',
           y: 'Mision_hs',
           fill: 'Nacionalidad',
-          sort: 'Mision_hs',
+          sort: 'Mision_hsc',
           
         }),
       ],
