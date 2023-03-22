5027d7c2367a1f2b27223aad2ef2b6126fc43562
Nanu Kissner <nadiakissner17@gmail.com>
2023-03-22 10:08:29 -0300

    hola

--- dataviz_1/script.js
+++ dataviz_1/script.js
@@ -2,14 +2,30 @@
 d3.csv("astronautas.csv", d3.autoType).then((data) => {
   console.log(data)
   let chart = Plot.plot({
-    marks: [Plot.dot(data, { x: "edad_mision", y: "mision_hs" })],
+    marks: [
+      Plot.frame(),
+      Plot.dot(data, { 
+      x: "anio_mision", 
+      y: "mision_hs",
+      stroke: "genero",
+      fill: "genero",
+      fillOpacity: 0.5,
+      r: 2, 
+    })],
+    width: 700,
+    height: 400,
+    color: {
+      legend: true,
+      scheme: "category10",
+    },
     x: {
+      label: "Año",
       grid: true,
       line: true,
-      zero: true,
       nice: true,
     },
     y: {
+      label: "Horas de misión",
       nice: true,
       line: true,
       grid: true,
--- dataviz_3/script.js
+++ dataviz_3/script.js
@@ -1,21 +1,31 @@
 
-
 d3.csv('astronautas.csv', d3.autoType).then(data => {
     console.log(data)
-    
     let chart = Plot.plot({
+      x: {
+        label: "Año"
+      },
+      y: {
+        grid: true,
+        label: "Cantidad de astronautas"
+      },
+      color: {
+      
+      },
       marks: [
-        Plot.barY(data, {
-          x: 'genero',
-          y: 'mision_hs',
-          fill: 'nacionalidad',
-          sort: 'genero',
-          
-        }),
-      ],
-      marginLeft: 70,
-      width: 300,
-    })
+        Plot.lineX(data, 
+          Plot.groupX(
+            {y: "count"}, 
+            {x: "anio_mision"}, 
+            )),
+        Plot.lineX(data, Plot.groupX({y: "count"}, {x: "anio_mision"})),
+        Plot.ruleY([0])
+        //Plot.line(data, {
+          //x: 'anio_mision',
+          //y: '',
+          //stroke: 'genero',
+          //sort: 'genero',
+      ]
+      })
     d3.select('#chart').append(() => chart)
-  })
-  
+    })
