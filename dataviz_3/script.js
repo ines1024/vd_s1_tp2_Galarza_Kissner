1e28e85d330ce38a69768247e03ae508cc34f8f3
ines galarza <inegalarza@gmail.com>
2023-03-22 08:21:21 -0300

    commit 10

--- dataviz_3/script.js
+++ dataviz_3/script.js
@@ -13,8 +13,38 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
           
         }),
       ],
-      marginLeft: 70,
-      width: 300,
+
+      width: 400,
+      height: 400,
+      inset: 5,
+      marginLeft: 160, 
+      marginBottom: 75,
+      marginTop: 30,
+      zero: true,
+
+      y: {
+        grid: true,
+        label: 'Horas de misión',
+        labelOffset: '100'
+        
+      },
+      x: {
+        
+        label: 'Género',
+        labelOffset: 40
+        
+      },
+      color: {
+        scheme: 'tableau10',
+    
+      },
+      style: {
+        fontFamily: 'sans-serif',
+        fontSize: 12,
+        background: '#',
+        color: 'black',
+        padding: '16px',
+      },
     })
     d3.select('#chart').append(() => chart)
   })
