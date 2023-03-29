c68ab8ba8843ac293529b93677c06537aeed4d1c
ines galarza <inegalarza@gmail.com>
2023-03-29 09:15:48 -0300

    commit 18

--- dataviz_1/script.js
+++ dataviz_1/script.js
@@ -2,12 +2,8 @@
 d3.csv("astronautas.csv", d3.autoType).then((data) => {
   console.log(data)
   console.log(d => d.nacioanlidad === "U.S.S.R/Rusia")
-
-  let chart = {
-    type: 'pie',
-    data: d.nacioanlidad,
-
-  }
+  d3.pie()
+  
 
   d3.select("#chart").append(() => chart);
 });
